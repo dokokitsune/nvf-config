2dcddd3de0069987bbfadf16a93ee5ec29e83d23
Weston Wood <stakin101@gmail.com>
2026-01-23 21:10:40 -0800

    reworked flake.nix

--- flake.nix
+++ flake.nix
@@ -1,43 +1,32 @@
-{
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nvf.url = "github:notashelf/nvf";
+{inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nvf.url = "github:NotAShelf/nvf";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nvf,
-      ...
-    }@inputs:
-    let
-      forEachSystem = nixpkgs.lib.getAttrs [
+  outputs = {
+    nixpkgs,
+    flake-parts,
+    nvf,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      # Define supported systems
+      systems = [
         "x86_64-linux"
         "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
 
-    in
-    {
-      packages = forEachSystem (
-        system:
-        let
-          pkgs = inputs.nixpkgs.legacyPackages.${system};
-          configModule = import ./plugins;
-          neovimConfigured = inputs.nvf.lib.neovimConfiguration {
+      perSystem = {system, ...}: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default =
+          (nvf.lib.neovimConfiguration {
             inherit pkgs;
-
-            modules = [ configModule ];
-
-          };
-        in
-        {
-
-          neovim = neovimConfigured.neovim;
-          default = self.packages.${system}.neovim;
-        }
-      );
+            modules = [./plugins];
+          }).neovim;
+      };
     };
 }
